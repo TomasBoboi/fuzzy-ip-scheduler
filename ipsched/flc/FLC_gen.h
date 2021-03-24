089a3c2655e787335d03a693e9e62e4ccdae1365
Tomas-Adrian Boboi <boboitomas@gmail.com>
2021-03-24 23:48:24 +0200

    Updated documentation structure; Implemented the FLC and made it run properly; TODO: Experiments;

---
+++ ipsched/flc/FLC_gen.h
@@ -0,0 +1,14 @@
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class GeneratorFLC : public cSimpleModule
+{
+    private:
+        cMessage *sendMessageEvent;
+
+    protected:
+        simtime_t flc_time;
+        virtual void initialize();
+        virtual void handleMessage(cMessage *msg);
+};
